fa284b4466b2bf9ccf1417e6752478a11e1f4fa1
Kevin Zhang <kevinzhang3702@gmail.com>
2016-10-14 11:33:26 -0400

    I think I fixed all the errors

--- list.c
+++ list.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-typedef struct {
+typedef struct song_node{
   char name[256];
   char artist[256];
   song_node *next;
@@ -10,31 +10,41 @@ typedef struct {
 
 song_node * insert_front( song_node *current, char *n, char *art){
   song_node * newNode = (song_node *)malloc( sizeof(song_node) );
-  (*newNode).name = *n;
-  (*newNode).artist = *art;
+  //(*newNode).name = *n;
+  strcpy( (*newNode).name, *n);
+  //(*newNode).artist = *art;
+  strcpy( (*newNode).artist, *art);
   (*newNode).next = current;
   return newNode;
 }
 
 song_node * insert( song_node *current, char *n, char *art){
-  song_node * newNode = (song_nude *)malloc(sizeof(song_node) );
-  (*newNode).name = *n;
-  (*newNode).artist = *art;
+  song_node * newNode = (song_node *)malloc( sizeof(song_node) );
+  //(*newNode).name = *n;
+  strcpy( (*newNode).name, *n);
+  //(*newNode).artist = *art;
+  strcpy( (*newNode).artist, *art);
   song_node * frs = current;
-  while( strcmr( *current.artist, *newNode.artist ) <= 0 ){
-    current = *current.next;
+  while( strcmr( (*current).artist, (*newNode).artist ) <= 0 ){
+    current = (*current).next;
   }
-  song_node * temp = *current.next;
-  *current.next = newNode;
-  *newNode.next = temp;
+  song_node * temp = (*current).next;
+  (*current).next = newNode;
+  (*newNode).next = temp;
   return frs;
 }
 
 void print_list( song_node * head){
   printf("Begin: ");
   while(head != 0){
-    printf("[%s] by %s;", *head.name, *head.artist);
-    head = *head.next;
+    printf("[%s] by %s;", (*head).name, (*head).artist);
+    head = (*head).next;
   }
   printf(":End\n");
+}
+
+song_node * findSong( song_node * head, char * songName){
+  while( head != 0){
+   
+  }
 }
