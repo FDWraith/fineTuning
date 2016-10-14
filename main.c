281ff7c9382f9a41a2b8539d380572746444f48e
Stephan Doan <sdoan@stuy.edu>
2016-10-14 11:46:51 -0400

    mod main.c, added more test. no worky

--- main.c
+++ main.c
@@ -5,8 +5,31 @@
 
 int main(){
   //testing insert_front
-  song_node * t1 = (song_nude *)malloc(sizeof(song_node));
-  t1 = insert_front(t1, "Biotonic", "ani");
+  song_node * t1 = (song_node *)malloc(sizeof(song_node));
+  char s1[256], s2[256], s3[256], s4[256], a1[256], a2[256], a3[256], a4[256];
+  strcpy(s1, "Shards of Light");
+  strcpy(s2, "Fluquor");
+  strcpy(s3, "Alive");
+  strcpy(s4, "Biotonic");
+  strcpy(a1, "Arn Andersson");
+  strcpy(a2, "Onoken");
+  strcpy(a3, "Philip Lober");
+  strcpy(a4, "Ani");
+
+  strcpy(t1->name, s1);
+  strcpy(t1->artist, a1);
+  t1 = insert_front(t1, s2, a2);
   print_list(t1);
+
+  //testing insert
+  t1 = insert(t1, s3, a3);
+  print_list(t1);
+  t1 = insert(t1, s4, a4);
+  print_list(t1);
+
+  //testing 
+
+  //testing
+
   return 21;
 }
