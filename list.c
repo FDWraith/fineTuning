b6de9f96834da158405296f9767a60ee4d7dabb4
Kevin Zhang <kevinzhang3702@gmail.com>
2016-10-15 20:29:45 -0400

    Finished the linked list functions.

--- list.c
+++ list.c
@@ -8,7 +8,17 @@ typedef struct song_node{
   struct song_node *next;
 } song_node;
 
+char * lowerString(char * str){
+  int i = 0;
+  for(;i<strlen(str);i++){
+    str[i] = toLower(str[i]);
+  }
+  return str;
+}
+
 song_node * insert_front( song_node *current, char *n, char *art){
+  n = lowerString(n);
+  art = lowerString(art);
   song_node * newNode = (song_node *)malloc( sizeof(song_node) );
   //(*newNode).name = *n;
   strcpy( (*newNode).name, n);
@@ -19,6 +29,8 @@ song_node * insert_front( song_node *current, char *n, char *art){
 }
 
 song_node * insert( song_node *current, char *n, char *art){
+  n = lowerString(n);
+  art = lowerString(art);
   song_node * newNode = (song_node *)malloc( sizeof(song_node) );
   //(*newNode).name = *n;
   strcpy( (*newNode).name, n);
@@ -44,6 +56,7 @@ void print_list( song_node * head){
 }
 
 song_node * findSong( song_node * head, char * songName){
+  songName = lowerString(songName);
   while( head != 0){
     if( strcmr( songName, (*head).name ) == 0){
       return head;
@@ -54,3 +67,72 @@ song_node * findSong( song_node * head, char * songName){
   printf("Song Not Found\n");
   return 0;//Cannot be found.
 }
+
+song_node * findArtist( song_node * head, char * artistName ){
+  artistName = lowerString(artistName);
+  while( head != 0){
+    if( strcmr( artistName, (*head).artist ) == 0){
+      return head;
+    }else{
+      head = (*head).next;
+    }
+  }
+  printf("Artist Not Found\n");
+  return 0;
+}
+
+int listLength( song_node * head){
+  int n = 0;
+  while( head != 0){
+    n++;
+    head = (*head).next;
+  }
+  return n;
+} 
+
+song_node * randomNode( song_node * head){
+  int len = listLength(head);
+  int r = (int)(rand()) * len;
+  while( r != 0 && head != 0){
+    r--;
+    head = (*head).next;
+  }//while loop ends, r must be 0
+
+  if(head == 0){
+    printf("You have an error on your hands\n");
+    return 0;
+  }else{
+    return head;
+  }
+}
+
+void removeNode( song_node * chosen ){
+  //not sure how to do this one.
+}
+
+song_node * removeSong( song_node * head, char * songName ){
+  if( head == 0){
+    printf("You're in the wrong neighborhood, kid\n");
+    return 0;
+  }else{
+    songName = lowerString(songName);
+    if( strcmr( (*((*head).next)).name, songName) == 0 ){
+      song_node temp = (*head).next;
+      (*head).next = (*temp).next;
+      return temp;
+    }else{
+      return removeSong( (*head).next, songName );
+    }
+  }
+}
+
+song_node * freeNodes( song_node * head ){
+  if( head != 0){
+    free(head);
+    head = freeNodes( (*head).next );
+  }else{
+    free(head);
+  }
+  return head;
+}
+
