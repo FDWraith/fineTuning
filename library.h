e4ff2e0e9052a0cb3423e247028ecd36d6187a8c
Kevin Zhang <kevinzhang3702@gmail.com>
2016-10-16 18:21:50 -0400

    Added the function definitions to library.h

--- library.h
+++ library.h
@@ -1,7 +1,13 @@
 typedef struct library{
   song_node * table[26];
 } library;
-void addSong( library * lib, char * songName, char * artistName);
-song_node * searchSong( library * lib, char * songName);
-song_node * searchArtist( library * lib, char * artistName);
+void addSong( library * lib, char * songName, char * artistName);//adds a song to the library.
+song_node * searchSong( library * lib, char * songName);//finds the first match given a song name
+song_node * searchArtist( library * lib, char * artistName);//finds the first match given an artist name.
+void printLetter( library * lib, char letter);//prints the list of artists given the first letter.
+void printArtist( library * lib, char * artistName);//prints out the list of songs that the artist produced.
+void printLibrary(library * lib);//prints out the library.
+void shuffleList( library * lib);//prints out a series of random songs chosen from the library.
+void deleteSong( library * lib, char * songName, char * artistName);//deletes a song from the library.
+void deleteLibrary( library * lib);//deletes the whole library.
 
